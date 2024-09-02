ab6a61c62c3d32d35e58a3ab455df7d85d07f8e2
yoooooooosi <138772092+yoooooooosi@users.noreply.github.com>
2024-09-02 17:00:30 +0800

    初始化Spring Boot Project

---
+++ src/main/java/com/amber/springbootmall/SpringBootMallApplication.java
@@ -0,0 +1,13 @@
+package com.amber.springbootmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMallApplication.class, args);
+    }
+
+}
